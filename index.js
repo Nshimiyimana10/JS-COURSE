a0f62b50ad2215313efa09e036b0adb7190f587e
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-08-23 13:04:28 -0700

    takeaway-2 added

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ document.querySelectorAll(".btn").forEach((button)=>{
     })
   })
 
- // document.write("Hello JS")//
- // alert("Dealing with JS")//
-  console.log("Learning Journey of JS")
+ document.write("Hello JS")
+ alert("Dealing with JS")
+console.log("Learning Journey of JS")
+
+console.log("Hello everyone");
+console.log("Learning JS require endurance");
+console.log("Let'us put our effort together");
+console.log("we can achieve a lot in life")
+
+//Single line comment
+
+console.log("This is single line comment");
+
+//Multi-line comment
+
+/*console.log("This is single line comment");
+console.log("This is single line comment");
+console.log("This is single line comment");
+console.log("This is single line comment");*/
+
