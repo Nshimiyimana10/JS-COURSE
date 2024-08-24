73adeb313b0d030611fb1fa1bca388663cee4fde
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-08-24 10:05:38 -0700

    take-away3

--- index.js
+++ index.js
@@ -24,3 +24,24 @@ console.log("This is single line comment");
 console.log("This is single line comment");
 console.log("This is single line comment");*/
 
+
+//Variable declaration
+
+let name ="Nshimiyimana";
+ let car ="Ferari";
+      car ="Limousine";/*variables declared with let and var can be re-assigned 
+                          new value*/
+
+console.log(name);
+console.log(car);
+const fruit ="pineapple";
+console.log(fruit);
+      fruit="mango";/*variables declared with const can't be re-assigned 
+      new value it shows error in console of the browser*/
+      
+
+
+
+
+
+
